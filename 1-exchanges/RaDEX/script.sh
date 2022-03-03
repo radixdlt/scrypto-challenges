b7be0277276801885b9c8d7cdf633da2f3e2d36b
0xOmarA <OmarAbdulla7@hotmail.com>
2022-03-03 21:01:10 +0300

    Added an example removing liquidity

--- 1-exchanges/RaDEX/script.sh
+++ 1-exchanges/RaDEX/script.sh
@@ -9,24 +9,24 @@ resim reset
 
 # Creating a new account. This account will be mainly used for the creation of the tokens
 OP1=$(resim new-account)
-PUB_KEY1=$(echo "$OP1" | sed -nr "s/Public key: ([[:alnum:]_]+)/\1/p")
-ACC_ADDRESS1=$(echo "$OP1" | sed -nr "s/Account address: ([[:alnum:]_]+)/\1/p")
+export PUB_KEY1=$(echo "$OP1" | sed -nr "s/Public key: ([[:alnum:]_]+)/\1/p")
+export ACC_ADDRESS1=$(echo "$OP1" | sed -nr "s/Account address: ([[:alnum:]_]+)/\1/p")
 echo "Created account: $ACC_ADDRESS1; Public Key: $PUB_KEY1"
 
 # Creating a number of additional accounts for the users that will be using the DEX.
 OP2=$(resim new-account)
-PUB_KEY2=$(echo "$OP2" | sed -nr "s/Public key: ([[:alnum:]_]+)/\1/p")
-ACC_ADDRESS2=$(echo "$OP2" | sed -nr "s/Account address: ([[:alnum:]_]+)/\1/p")
+export PUB_KEY2=$(echo "$OP2" | sed -nr "s/Public key: ([[:alnum:]_]+)/\1/p")
+export ACC_ADDRESS2=$(echo "$OP2" | sed -nr "s/Account address: ([[:alnum:]_]+)/\1/p")
 echo "Created account: $ACC_ADDRESS2; Public Key: $PUB_KEY2"
 
 OP3=$(resim new-account)
-PUB_KEY3=$(echo "$OP3" | sed -nr "s/Public key: ([[:alnum:]_]+)/\1/p")
-ACC_ADDRESS3=$(echo "$OP3" | sed -nr "s/Account address: ([[:alnum:]_]+)/\1/p")
+export PUB_KEY3=$(echo "$OP3" | sed -nr "s/Public key: ([[:alnum:]_]+)/\1/p")
+export ACC_ADDRESS3=$(echo "$OP3" | sed -nr "s/Account address: ([[:alnum:]_]+)/\1/p")
 echo "Created account: $ACC_ADDRESS3; Public Key: $PUB_KEY3"
 
 OP4=$(resim new-account)
-PUB_KEY4=$(echo "$OP4" | sed -nr "s/Public key: ([[:alnum:]_]+)/\1/p")
-ACC_ADDRESS4=$(echo "$OP4" | sed -nr "s/Account address: ([[:alnum:]_]+)/\1/p")
+export PUB_KEY4=$(echo "$OP4" | sed -nr "s/Public key: ([[:alnum:]_]+)/\1/p")
+export ACC_ADDRESS4=$(echo "$OP4" | sed -nr "s/Account address: ([[:alnum:]_]+)/\1/p")
 echo "Created account: $ACC_ADDRESS4; Public Key: $PUB_KEY4"
 
 # Switching the default account to account 1 and creating the sample tokens using this account
@@ -36,12 +36,12 @@ echo "Created tokens using account: $ACC_ADDRESS1"
 
 # Publishing the package to the local simulator
 PK_OP=$(resim publish "$SCRIPT_DIR")
-PACKAGE=$(echo "$PK_OP" | sed -nr "s/Success! New Package: ([[:alnum:]_]+)/\1/p")
+export PACKAGE=$(echo "$PK_OP" | sed -nr "s/Success! New Package: ([[:alnum:]_]+)/\1/p")
 echo "Created package: $PACKAGE"
 
 # Creating the RaDEX component
 CP_OP=$(resim run "$SCRIPT_DIR/transactions/component_creation.rtm")
-COMPONENT=$(echo "$CP_OP" | sed -nr "s/└─ Component: ([[:alnum:]_]+)/\1/p")
+export COMPONENT=$(echo "$CP_OP" | sed -nr "s/└─ Component: ([[:alnum:]_]+)/\1/p")
 echo "Created component: $COMPONENT"
 
 # Creating the liquidity pools
@@ -66,3 +66,8 @@ echo "Complex token swap ADA -> DOGE done"
 resim set-default-account $ACC_ADDRESS4 $PUB_KEY4 > /dev/null
 resim run "$SCRIPT_DIR/transactions/swap_BTC_for_USDT_and_add_liquidity.rtm" > /dev/null
 echo "Complex token swap BTC -> USDT and providing liquidity done"
+
+# Switching to account 1 to remove all of the BTC/USDT liquidity.
+resim set-default-account $ACC_ADDRESS1 $PUB_KEY1 > /dev/null
+resim run "$SCRIPT_DIR/transactions/remove_BTC_USDT_liquidity.rtm" > /dev/null
+echo "Removing BTC/USDT liquidity done"
