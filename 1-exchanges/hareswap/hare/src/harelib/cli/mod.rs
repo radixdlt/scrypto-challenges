a3ed8549ba82e781430b99367dc0840229bb1dc4
devmannic <82296715+devmannic@users.noreply.github.com>
2022-03-08 01:04:21 -0500

    CLI and testing progress

--- 1-exchanges/hareswap/hare/src/harelib/cli/mod.rs
+++ 1-exchanges/hareswap/hare/src/harelib/cli/mod.rs
@@ -1,35 +1,87 @@
 use clap::Parser;
-use scrypto::buffer::scrypto_encode;
-use std::fs::read_to_string;
-use std::fs::write;
+use clap::Subcommand;
+ use scrypto::buffer::scrypto_encode;
+// use std::fs::read_to_string;
+use std::fs;
 use std::path::PathBuf;
-use transaction_manifest::compile;
 
-/// Radix transaction manifest compiler
+use radix_engine::ledger::*;
+//use radix_engine::transaction::*;
+use scrypto::types::EcdsaPublicKey;
+use scrypto::utils::sha256;
+use simulator::resim::*;
+use simulator::ledger::*;
+
+//use hareswap::api::*;
+
+/// hareswap CLI
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None, name = "hare")]
+#[clap(author, version, about, long_about = None)]
 pub struct Args {
-    /// Path to the output file
-    #[clap(short, long)]
-    output: PathBuf,
+    #[clap(subcommand)]
+    command: Command,
+}
 
-    /// Input file
-    #[clap(required = true)]
-    input: PathBuf,
+#[derive(Subcommand, Debug)]
+pub enum Command {
+    NewKeyPair(NewKeyPair),
 }
 
 #[derive(Debug)]
 pub enum Error {
     IoError(std::io::Error),
-    CompileError(transaction_manifest::CompileError),
+    ResimError(simulator::resim::Error)
 }
 
 pub fn run() -> Result<(), Error> {
     let args = Args::parse();
 
-    let content = read_to_string(args.input).map_err(Error::IoError)?;
-    let transaction = compile(&content).map_err(Error::CompileError)?;
-    write(args.output, scrypto_encode(&transaction)).map_err(Error::IoError)?;
+    match args.command {
+        Command::NewKeyPair(cmd) => cmd.run()
+    }
+}
 
-    Ok(())
+/// generate new key pair for offline signing and online verification
+#[derive(Parser, Debug)]
+pub struct NewKeyPair {
+    public_key: PathBuf,
+    private_key: PathBuf,
+}
+
+impl NewKeyPair {
+    pub fn run(&self) -> Result<(), Error> {
+        let mut ledger = RadixEngineDB::with_bootstrap(get_data_dir().map_err(Error::ResimError)?);
+
+        let (public_bytes, private_bytes) = new_public_private_pair(&mut ledger);
+
+        fs::write(&self.public_key, public_bytes).map_err(Error::IoError)?;
+        fs::write(&self.private_key, private_bytes).map_err(Error::IoError)?;
+
+        Ok(())
+    }
+}
+
+use k256::{
+    ecdsa::SigningKey,
+};
+//use rand_core::OsRng; // requires 'getrandom' feature
+
+pub fn new_public_private_pair(ledger: &mut RadixEngineDB) -> (Vec<u8>, Vec<u8>) {
+    // WARNING this is insecure, used for testing with deterministic keys (similar to how new_public_key works)
+    let mut private_raw = [0u8; 32];
+    private_raw[..].copy_from_slice(sha256(ledger.get_nonce().to_string()).as_ref());
+    ledger.increase_nonce();
+    let signing_key = SigningKey::from_bytes(&private_raw).expect("unable to create signing key (this should not happen)");
+
+    let verifying_key = signing_key.verifying_key();
+    let compressed_point = verifying_key.to_bytes();
+    let mut public_raw = [0u8; 33];
+    public_raw[..].copy_from_slice(&compressed_point);
+    let public_key: EcdsaPublicKey = EcdsaPublicKey(public_raw);
+    let public_key_encoded = scrypto_encode(&public_key);
+
+    (
+        public_key_encoded,
+        signing_key.to_bytes().to_vec(),
+    )
 }
--- 1-exchanges/hareswap/testing/setup.sh
+++ 1-exchanges/hareswap/testing/setup.sh
@@ -8,32 +8,38 @@ resim publish ../target/wasm32-unknown-unknown/release/hareswap.wasm
 PACKAGE=0124c5afc33cf45c06633d8fc0b0dfba2c82f14ec82ff7eb13483c
 
 # 0.0 taker
+# baseline taker account
 resim new-account
 ACCOUNT1=02e1bbfc1eb7b1fa431c9ae0b1f7ee66660a52adf2739f621ce424
 ACCOUNT1_PUBKEY=006b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b
-
+# baseline "T" tokens
 resim new-token-fixed 1000
 T=03d527faee6d0b91e7c1bab500c6a986e5777a25d704acc288d542
 
 # 0.1 taker
+# hareswap-specific: create a taker_auth to prevent frontrunning when submitted maker-signed orders
 resim new-badge-fixed 1
 TAKER_AUTH=0347dfe3a58e8a630305f2f3df82949cd70ce49e2cde097b259f8d
 
 # 0.0 maker
+# maker baseline new account
 resim new-account
 ACCOUNT2=022ab83d6a41454e5cf04a5442cf70acf5fb19af0c8938fadfe141
 ACCOUNT2_PUBKEY=00ef2d127de37b942baad06145e54b0c619a1f22327b2ebbcfbec78f5564afe39d
 resim set-default-account $ACCOUNT2 $ACCOUNT2_PUBKEY
-
+# baseline "M" tokens
 resim new-token-fixed 1000
 M=0398652f4eb36dd2067191845deb68e54771074f35dc78fbf820a4
 
 # 0.1 maker: account setup
+# new badge for access to CustodialAccount
 resim new-badge-fixed 2
 # CallFunction { package_address: 010000000000000000000000000000000000000000000000000001, blueprint_name: "System", function: "new_resource", args: [Enum(0u8, {0u8}), HashMap<String, String>(), 0u64, 0u64, HashMap<Address, U64>(), Some(Enum(0u8, {Decimal("2")}))] }
 MAKER_ACCOUNT_AUTH=031773788de8e4d2947d6592605302d4820ad060ceab06eb2d4711
+# create the accont
 resim call-function $PACKAGE "CustodialAccount" "new_easy" $MAKER_ACCOUNT_AUTH
 MAKER_ACCOUNT=02d9e04ba122de13a58f80ea7a06a0e1aad665d23cbeb124c3c286
+# put half the M in there ready to trade
 resim transfer 500,$M $MAKER_ACCOUNT
 
 ### probably break the rest into seperate file later
@@ -41,9 +47,27 @@ resim transfer 500,$M $MAKER_ACCOUNT
 # 0.2 Maker setup
 
 # TODO: get a public key off-ledger
-pubkey=0x1234
-resim call-function $PACKAGE "Maker" "instantiate" $pubkey EMPTY_BUCKET $MAKER_ACCOUNT bucket_of_MAKER_ACCOUNT_AUTH
-MAKER_COMPONENT=...
+
+### Ahhhh.  resim can't handle empty buckets, and RTMs cannot return Component addresses
+### so NEED to do this part in code, ie. ./hare maker --setup env_file
+### needs to know $M $MAKER_ACCOUNT $_MAKER_ACCOUNT_AUTH pubkey callback_auth_bucket
+### well WAIT, can use rtm, then do a resim command to look at the latest component!
+
+# hare cli would need $PACKAGE $ACCOUNT2 $ACCOUNT2_KEY $MAKER_ACCOUNT $MAKER_ACCOUNT_AUTH
+# and it would need to return the new Maker component address
+
+resim show-ledger > ledger_before.txt
+`hare new_key_pair --path ./maker`
+pubkey=`cat ./maker.pub`
+cat > maker_setup.rtm  <<EOF
+METHOD_CALL $ACCOUNT2 "withdraw" Decimal("1") Address("$MAKER_ACCOUNT_AUTH")
+TAKE_FROM_WORKTOP Decimal("1") Address("$MAKER_ACCOUNT_AUTH") Bucket("maker_account_auth")
+TAKE_FROM_WORKTOP Decimal("0") Address("030000000000000000000000000000000000000000000000000004") Bucket("empty_xrd")
+FUNCTION_CALL $PACKAGE "Maker" "instantiate" $pubkey" Bucket("empty_xrd") Address("$MAKER_ACCOUNT") Bucket("maker_account_auth")
+EOF
+resim show-ledger > ledger_after.txt
+MAKER_COMPONENT=$(diff ledger_before.txt ledger_after.txt | head -n1 | cut -d' ' -f2)
+mv ledger_after.txt ledger_before.txt
 
 
 #resim call-function $HARESWAP_PACKAGE "Maker" "instantiate" public_key bucket component auth_bucket
