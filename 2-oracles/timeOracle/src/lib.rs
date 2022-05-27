69e5ce2251f70406dbc0a94a2529789c7e192eab
Miro <56092186+MiroLiebschner@users.noreply.github.com>
2022-05-27 20:49:32 +0200

    Finished

--- 2-oracles/timeOracle/src/lib.rs
+++ 2-oracles/timeOracle/src/lib.rs
@@ -7,15 +7,16 @@ blueprint! {
         fee_vault: Vault,
         // the adming badge that is used to empty the fee_vault
         admin_badge_def: ResourceAddress,
-        time_string: String,  
+        // the time string UNIX format
+        time_string: String, 
+        //the counter that is used to check if the next update_time() is paid for 
+        paid_requests: Decimal,  
 
     }
 
     impl TimeOracle {
-        // Implement the functions and methods which will manage those resources and data
-        
         // This is a function, and can be called directly on the blueprint once deployed
-        pub fn instantiate_hello() -> (ComponentAddress, Bucket) {
+        pub fn instantiate_time_oracle() -> (ComponentAddress, Bucket) {
          
         // Create the admin badges
         let badges: Bucket = ResourceBuilder::new_fungible()
@@ -28,29 +29,50 @@ blueprint! {
                 fee_vault: Vault::new(RADIX_TOKEN),
                 admin_badge_def: badges.resource_address(),
                 time_string: String::new(),
+                paid_requests: dec!(0),
             }
             .instantiate();
 
             // Define the access rules for this blueprint.
         let access_rules = AccessRules::new()
-        .method("do_admin_task", rule!(require(badges.resource_address())));
-        
+        .method("collect_fees", rule!(require(badges.resource_address()))).default(rule!(allow_all));
 
+  
         // Return the component and the badges
         (component.add_access_check(access_rules).globalize(), badges)
 
-        
         }
+
+        // allows users to pay for time updates
+        pub fn pay_for_update_time(&mut self, mut payment: Bucket) -> Bucket {
+            // Put 1 (xrd) in the fee vault 
+            self.fee_vault.put(payment.take(dec!(1)));
+            // increase the number of outstanding paid_requests
+            self.paid_requests = self.paid_requests + dec!(1);
+            // returns bucket of xrd if too much was paid 
+            payment
+        }
+
         // Updates the time with the help of an off-chain API and the frontend build with he frontend sdk
-        pub fn update_time(&mut self){
-
+        pub fn update_time(&mut self, new_time: String){
+            // Check that there is at least one request that is paid for
+            assert!(self.paid_requests > dec!(1), "Need to pay for request first");
+            //Decrease counter 
+            self.paid_requests -= 1;
+            //Updates the time 
+            self.time_string = new_time;
         }
-        
-        pub fn do_admin_task(&self) {
-            // This method can only be called if the user
-            // presents a badge of the `admin_badge_def` resource definition
+
+        // returns the last UNIX time_string 
+        pub fn get_time(&self) -> String {
+           let time = self.time_string.clone();
+           time
         }
 
+        // collects fees. Can only be called by component owner (has admin_badge)
+        pub fn collect_fees(&mut self){
+            self.fee_vault.take_all();
+        }
     
     }
 }
