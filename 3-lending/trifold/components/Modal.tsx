b6c1b704cf2990dddd2b0e4ad8325d0dab909475
citricacid2 <61765149+citricacid2@users.noreply.github.com>
2022-07-18 16:00:26 -0400

    Adding everything from another folder

---
+++ 3-lending/trifold/components/Modal.tsx
@@ -0,0 +1,53 @@
+import { ReactNode } from "react";
+
+interface ModalProps {
+  title: string;
+  run: () => void;
+  cancel: () => void;
+  children: ReactNode;
+}
+
+const Modal = ({ title, run, cancel, children }: ModalProps) => {
+  // modal inspired by TailwindUI
+  return (
+    <div className="fixed z-10 inset-0">
+      <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0 backdrop-blur-xl">
+        <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3
+                  className="text-lg leading-6 font-medium text-gray-900"
+                  id="modal-title"
+                >
+                  {title}
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">{children}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <button
+              type="button"
+              onClick={run}
+              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Start
+            </button>
+            <button
+              type="button"
+              onClick={cancel}
+              className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
