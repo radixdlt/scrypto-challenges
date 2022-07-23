84f16544e169309b385ef9184c264e951f19331c
avincigu <arthur.vinciguerra@ens-lyon.fr>
2022-07-23 16:40:51 +0200

    added token tests

--- smaller-challenges/token2poche/src/lib.rs
+++ smaller-challenges/token2poche/src/lib.rs
@@ -11,7 +11,6 @@ blueprint! {
 
    impl Token2Poche {
        pub fn new(price_per_token: Decimal) -> (ComponentAddress, Bucket) {
-
             // Create the admin badge bucket
             let init_badge: Bucket = ResourceBuilder::new_fungible()
                 .divisibility(DIVISIBILITY_NONE)
@@ -23,6 +22,8 @@ blueprint! {
             let init_bucket: Bucket = ResourceBuilder::new_fungible()
                 .divisibility(DIVISIBILITY_MAXIMUM)
                 .metadata("name", "Token2Pochent")
+                .metadata("persoent -->", "ent")
+                .metadata("symbol", "ENT")
                 .metadata("team-member-1-ticket-number", "40893648296566556829001")
                 .metadata("team-member-2-ticket-number", "40893648296566556829001")
                 .initial_supply(100000);
@@ -58,7 +59,10 @@ blueprint! {
        }
 
        pub fn change_price(&mut self, price: Decimal) {
-		self.token_price = price;
+        if price > scrypto::math::Decimal(0)
+            {
+                self.token_price = price;
+            }
        }
    }
 }
--- smaller-challenges/token2poche/tests/lib.rs
+++ smaller-challenges/token2poche/tests/lib.rs
@@ -3,30 +3,75 @@ use radix_engine::transaction::*;
 use scrypto::prelude::*;
 
 #[test]
-fn test_hello() {
+fn test_token2poche() {
     // Set up environment.
     let mut ledger = InMemorySubstateStore::with_bootstrap();
     let mut executor = TransactionExecutor::new(&mut ledger, false);
-    let (pk, sk, account) = executor.new_account();
+    let (admin_pk, admin_sk, admin_account) = executor.new_account();
+    let (random_pk, random_sk, random_account) = executor.new_account();
     let package = executor.publish_package(compile_package!()).unwrap();
 
-    // Test the `instantiate_hello` function.
+
+    // Test the `new` function.
     let transaction1 = TransactionBuilder::new()
-        .call_function(package, "Hello", "instantiate_hello", args![])
-        .build(executor.get_nonce([pk]))
-        .sign([&sk]);
+        .call_function(package, "Token2Poche", "new", args![dec!(1000)])
+        .build(executor.get_nonce([admin_pk]))
+        .sign([&admin_sk]);
     let receipt1 = executor.validate_and_execute(&transaction1).unwrap();
     println!("{:?}\n", receipt1);
     assert!(receipt1.result.is_ok());
 
-    // Test the `free_token` method.
     let component = receipt1.new_component_addresses[0];
+    let admin_badge = receipt1.new_resource_addresses[0];
+
+    // Test the buy function with random account.
+    let transaction5 = TransactionBuilder::new()
+        .call_method(component, "buy", args![dec!(2)])
+        .call_method_with_all_resources(random_account, "deposit_batch")
+        .build(executor.get_nonce([random_pk]))
+        .sign([&random_sk]);
+    let receipt5 = executor.validate_and_execute(&transaction5).unwrap();
+    println!("{:?}\n", receipt5);
+    assert!(receipt5.result.is_ok());
+
+
+    // Test the `change_price` function with admin_account`.
     let transaction2 = TransactionBuilder::new()
-        .call_method(component, "free_token", args![])
-        .call_method_with_all_resources(account, "deposit_batch")
-        .build(executor.get_nonce([pk]))
-        .sign([&sk]);
+        .call_method(component, "change_price", args![dec!(1)])
+        .call_method_with_all_resources(admin_account, "deposit_batch")
+        .build(executor.get_nonce([admin_pk]))
+        .sign([&admin_sk]);
     let receipt2 = executor.validate_and_execute(&transaction2).unwrap();
     println!("{:?}\n", receipt2);
     assert!(receipt2.result.is_ok());
+
+    // Test the `change_price` function with the random account.
+    let failed_transaction1 = TransactionBuilder::new()
+        .call_method(component, "change_price", args![dec!(5)])
+        .call_method_with_all_resources(random_account, "deposit_batch")
+        .build(executor.get_nonce([random_pk]))
+        .sign([&random_sk]);
+    let failed_receipt1 = executor.validate_and_execute(&failed_transaction1).unwrap();
+    println!("{:?}\n", failed_receipt1);
+    assert!(failed_receipt1.result.is_err());
+
+    // Test the withdraw function with admin account.
+    let transaction3 = TransactionBuilder::new()
+        .call_method(component, "withdraw_funds", args![])
+        .call_method_with_all_resources(admin_account, "deposit_batch")
+        .build(executor.get_nonce([admin_pk]))
+        .sign([&admin_sk]);
+    let receipt3 = executor.validate_and_execute(&transaction3).unwrap();
+    println!("{:?}\n", receipt3);
+    assert!(receipt3.result.is_ok());
+
+    // Test the withdraw function with random account.
+    let failed_transaction2 = TransactionBuilder::new()
+        .call_method(component, "withdraw_funds", args![])
+        .call_method_with_all_resources(random_account, "deposit_batch")
+        .build(executor.get_nonce([random_pk]))
+        .sign([&random_sk]);
+    let failed_receipt2 = executor.validate_and_execute(&failed_transaction2).unwrap();
+    println!("{:?}\n", failed_receipt2);
+    assert!(failed_receipt2.result.is_err());
 }
