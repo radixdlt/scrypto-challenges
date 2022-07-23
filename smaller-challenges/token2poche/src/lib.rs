c615e157a6d4e5fb2a79004a21c643ab288a04eb
theodoreconrad <77613665+theodoreconrad@users.noreply.github.com>
2022-07-23 15:39:29 +0200

    3 functions ent

--- smaller-challenges/token2poche/src/lib.rs
+++ smaller-challenges/token2poche/src/lib.rs
@@ -5,25 +5,26 @@ blueprint! {
    struct Token2Poche {
         token_supply: Vault,
         collected_xrd: Vault,
-        admin_badge: ResourceDef,
+        admin_badge: ResourceAddress,
         token_price: Decimal
    }
 
    impl Token2Poche {
        pub fn new(price_per_token: Decimal) -> (ComponentAddress, Bucket) {
-           // Your `new` implementation goes here.
        }
 
        pub fn buy(&mut self, funds: Bucket) -> Bucket {
-           // Your `buy` implementation goes here.
+		let amount : Decimal = funds.amount() / self.token_price;
+		self.collected_xrd.put(funds.take(funds.amount()));
+		return self.collected_xrd.take(amount)
        }
 
        pub fn withdraw_funds(&mut self, amount: Decimal) -> Bucket {
-           // Your `withdraw_funds` implementation goes here.
+		return self.collected_xrd.take(amount)
        }
 
        pub fn change_price(&mut self, price: Decimal) {
-           // Your `change_price` implementation goes here.
+		self.token_price = price;
        }
    }
 }
