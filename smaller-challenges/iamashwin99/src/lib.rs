7206a61455ca4cb509587d3020e7a3f7c18db629
iamashwin99 <f20171034@goa.bits-pilani.ac.in>
2022-07-23 14:35:13 +0200

    added seller badge

--- smaller-challenges/iamashwin99/src/lib.rs
+++ smaller-challenges/iamashwin99/src/lib.rs
@@ -8,19 +8,25 @@ blueprint! {
     }
 
     impl TokenSale {
-        pub fn new(price_per_token: Decimal) -> ComponentAddress {
+        pub fn new(price_per_token: Decimal) -> (ComponentAddress,Bucket) {
             let bucket: Bucket = ResourceBuilder::new_fungible()
                 .metadata("name", "Useful Token")
                 .metadata("symbol", "USEFUL")
-                .initial_supply(1_000);
+                .initial_supply(100_000);
+            let seller_badge: Bucket= ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
+                .metadata("name", "Seller Badge")
+                .metadata("symbol", "SEL")
+                .initial_supply(1);
 
-            Self {
+            let component_address: ComponentAddress = Self {
                 useful_tokens_vault: Vault::with_bucket(bucket),
                 xrd_tokens_vault: Vault::new(RADIX_TOKEN), 
                 price_per_token: price_per_token
             }
             .instantiate()
-            .globalize()
+            .globalize();
+            (component_address,seller_badge)
         }
 
         pub fn buy(&mut self, funds: Bucket) -> Bucket {
@@ -29,7 +35,7 @@ blueprint! {
             self.useful_tokens_vault.take(purchase_amount)
         }
         pub fn withdraw_funds(&mut self, amount: Decimal) -> Bucket {
-             self.xrd_tokens_vault.take(amount);
+             self.xrd_tokens_vault.take(amount)
 
         }
         
