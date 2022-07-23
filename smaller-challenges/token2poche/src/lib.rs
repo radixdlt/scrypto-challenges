ee5c6acf08b5fa724b2f155c4d80b0974f816331
avincigu <arthur.vinciguerra@ens-lyon.fr>
2022-07-23 16:01:16 +0200

    token init added ent

--- smaller-challenges/token2poche/src/lib.rs
+++ smaller-challenges/token2poche/src/lib.rs
@@ -11,12 +11,46 @@ blueprint! {
 
    impl Token2Poche {
        pub fn new(price_per_token: Decimal) -> (ComponentAddress, Bucket) {
+
+            // Create the admin badge bucket
+            let init_badge: Bucket = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_NONE)
+                .metadata("name","admin badge")
+                .burnable(rule!(allow_all), LOCKED)
+                .initial_supply(1);
+
+            // Create the token bucket
+            let init_bucket: Bucket = ResourceBuilder::new_fungible()
+                .divisibility(DIVISIBILITY_MAXIMUM)
+                .metadata("name", "Token2Pochent")
+                .metadata("team-member-1-ticket-number", "40893648296566556829001")
+                .metadata("team-member-2-ticket-number", "40893648296566556829001")
+                .initial_supply(100000);
+
+            // Create the access rules
+            let rulze: AccessRules = AccessRules::new()
+                .method("withdraw_funds", rule!(require(init_badge.resource_address())))
+                .method("change_price", rule!(require(init_badge.resource_address())))
+                .default(rule!(allow_all));
+
+            // Instantiate the component
+            let comp = Self {
+                token_supply: Vault::with_bucket(init_bucket),
+                collected_xrd: Vault::new(RADIX_TOKEN),
+                admin_badge: init_badge.resource_address(),
+                token_price: price_per_token
+            }
+            .instantiate()
+            .add_access_check(rulze)
+            .globalize();
+
+            (comp, init_badge)
        }
 
        pub fn buy(&mut self, funds: Bucket) -> Bucket {
 		let amount : Decimal = funds.amount() / self.token_price;
-		self.collected_xrd.put(funds.take(funds.amount()));
-		return self.collected_xrd.take(amount)
+		self.collected_xrd.put(funds);
+		return self.token_supply.take(amount)
        }
 
        pub fn withdraw_funds(&mut self, amount: Decimal) -> Bucket {
