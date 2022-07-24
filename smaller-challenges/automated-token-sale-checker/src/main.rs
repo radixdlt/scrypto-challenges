a09093d6098544e5331f35677a2a23c695c32766
0xOmarA <OmarAbdulla7@hotmail.com>
2022-07-25 02:07:53 +0300

    Add more validation ot the automated checking

--- smaller-challenges/automated-token-sale-checker/src/main.rs
+++ smaller-challenges/automated-token-sale-checker/src/main.rs
@@ -4,5 +4,8 @@ mod utils;
 use test_runner::TokenSaleTestRunner;
 
 pub fn main() {
-    let mut token_sale_test_runner: TokenSaleTestRunner = TokenSaleTestRunner::new("").unwrap();
+    let mut token_sale_test_runner: TokenSaleTestRunner = TokenSaleTestRunner::new("")
+    .unwrap();
+
+    println!("Validation response: {:?}", token_sale_test_runner.validate_token_sale_blueprint());
 }
--- smaller-challenges/automated-token-sale-checker/src/test_runner.rs
+++ smaller-challenges/automated-token-sale-checker/src/test_runner.rs
@@ -123,6 +123,19 @@ impl TokenSaleTestRunner {
         }
     }
 
+    /// Gets the amount of tokens that an account has
+    pub fn account_balance(
+        &self,
+        component_address: &ComponentAddress,
+        resource_address: &ResourceAddress,
+    ) -> Decimal {
+        match self.account_vault(component_address, resource_address)
+        {
+            Some(vault) => vault.total_amount(),
+            None => Decimal::zero(),
+        }
+    }
+
     /// Executes a transaction against the current substate store
     pub fn execute_transaction(
         &mut self,
@@ -154,18 +167,34 @@ impl TokenSaleTestRunner {
     /// Validates that the token sale blueprint functions according to the rules of the challenge.
     pub fn validate_token_sale_blueprint(&mut self) -> Result<Vec<String>, PackageError> {
         let blueprint_name: String = self.validate_package_has_one_blueprint()?;
+        
+        let price_per_token: Decimal = dec!("10");
         let (
             token_sale_component_address,
             team_token_resource_address,
             seller_badge_resource_address,
         ): (ComponentAddress, ResourceAddress, ResourceAddress) =
-            self.validate_function_new(&blueprint_name)?;
+            self.validate_function_new(&blueprint_name, price_per_token)?;
         let ticket_numbers: Vec<String> =
             self.validate_team_token_metadata(&team_token_resource_address)?;
 
         self.validate_buy_method_produces_correct_amount(
             &token_sale_component_address,
             &team_token_resource_address,
+            price_per_token
+        )?;
+        
+        self.validate_non_seller_can_not_withdraw_funds(&token_sale_component_address)?;
+        self.validate_seller_can_withdraw_funds(&token_sale_component_address, &seller_badge_resource_address)?;
+        
+        let new_price: Decimal = dec!("100");
+        self.validate_non_seller_can_not_change_price(&token_sale_component_address, new_price)?;
+        self.validate_seller_can_change_price(&token_sale_component_address, &seller_badge_resource_address, new_price)?;
+
+        self.validate_buy_method_produces_correct_amount(
+            &token_sale_component_address,
+            &team_token_resource_address,
+            new_price
         )?;
 
         Ok(ticket_numbers)
@@ -184,6 +213,7 @@ impl TokenSaleTestRunner {
     fn validate_function_new(
         &mut self,
         blueprint_name: &str,
+        price_per_token: Decimal,
     ) -> Result<(ComponentAddress, ResourceAddress, ResourceAddress), PackageError> {
         // Calling the `new` function on the package to instantiate a new component
         let instantiate_new_token_sale_transaction: SignedTransaction = TransactionBuilder::new()
@@ -191,7 +221,7 @@ impl TokenSaleTestRunner {
                 self.package_address,
                 blueprint_name,
                 "new",
-                args![dec!("10")],
+                args![price_per_token],
             )
             .call_method_with_all_resources(self.account.component_address, "deposit_batch")
             .build(0)
@@ -230,12 +260,13 @@ impl TokenSaleTestRunner {
                     self.resource_manager(resource_address_2),
                 );
 
-                if resource_manager_1.total_supply() == dec!("1000") {
+                let amount_to_find: Decimal = dec!("100000");
+                if resource_manager_1.total_supply() == amount_to_find {
                     Ok((*resource_address_1, *resource_address_2))
-                } else if resource_manager_2.total_supply() == dec!("1000") {
+                } else if resource_manager_2.total_supply() == amount_to_find {
                     Ok((*resource_address_2, *resource_address_1))
                 } else {
-                    Err(PackageError::NoResourceWithSpecifiedSupply)
+                    Err(PackageError::NoResourceWithSpecifiedSupply(amount_to_find))
                 }
             }
             _ => Err(PackageError::NoResourceAddressAtIndex),
@@ -256,7 +287,7 @@ impl TokenSaleTestRunner {
         // Getting the resource manager of the resource address and the corresponding metadata
         let resource_manager: ResourceManager = self.resource_manager(resource_address);
         let metadata = resource_manager.metadata();
-        let elements: Vec<String> = (1..5)
+        let elements: Vec<String> = (1..20)
             .map(|i| metadata.get(&format!("team-member-{}-ticket-number", i)))
             .filter(|element| element.is_some())
             .map(|element| element.unwrap().clone())
@@ -273,11 +304,19 @@ impl TokenSaleTestRunner {
         &mut self,
         component_address: &ComponentAddress,
         team_token_resource_address: &ResourceAddress,
+        price_per_token: Decimal
     ) -> Result<(), PackageError> {
+        // Calculating the amount of tokens to expect back
+        let amount_to_spend: Decimal = dec!("100");
+        let amount_to_expect: Decimal = amount_to_spend / price_per_token;
+
+        // Getting the amount of tokens before running the transaction
+        let amount_before_tx: Decimal = self.account_balance(&self.account.component_address, team_token_resource_address);
+
         // Constructing the purchase transaction
         let buy_transaction: SignedTransaction = TransactionBuilder::new()
             .withdraw_from_account_by_amount(
-                dec!("100"),
+                amount_to_spend,
                 RADIX_TOKEN,
                 self.account.component_address,
             )
@@ -287,20 +326,141 @@ impl TokenSaleTestRunner {
             .call_method_with_all_resources(self.account.component_address, "deposit_batch")
             .build(0)
             .sign([&self.account.private_key]);
-        let buy_receipt: Receipt = self.execute_transaction(&buy_transaction)?;
+        self.execute_transaction(&buy_transaction)?;
 
-        let token_amount: Decimal = match self
-            .account_vault(&self.account.component_address, team_token_resource_address)
-        {
-            Some(vault) => vault.total_amount(),
-            None => Decimal::zero(),
-        };
+        let amount_after_tx: Decimal = self.account_balance(&self.account.component_address, team_token_resource_address);
 
-        if token_amount == dec!("10") {
+        if (amount_after_tx - amount_before_tx) == amount_to_expect {
             Ok(())
         } else {
             Err(PackageError::PriceBalanceMismatchError)
         }
+    }
+
+    /// Validates that when the caller is not in possession of the seller badge then they are unable to call the 
+    /// `withdraw_funds` method.
+    fn validate_non_seller_can_not_withdraw_funds(
+        &mut self,
+        component_address: &ComponentAddress,
+    ) -> Result<(), PackageError> {
+        // Constructing the withdraw funds transaction
+        let withdraw_funds_transaction: SignedTransaction = TransactionBuilder::new()
+            .take_from_worktop(RADIX_TOKEN, |builder, bucket_id| {
+                builder.call_method(
+                    component_address.clone(),
+                    "withdraw_funds",
+                    args![dec!("100")],
+                )
+            })
+            .call_method_with_all_resources(self.account.component_address, "deposit_batch")
+            .build(0)
+            .sign([&self.account.private_key]);
+        let withdraw_funds_receipt: Result<Receipt, PackageError> =
+            self.execute_transaction(&withdraw_funds_transaction);
+
+        if let Some(PackageError::TransactionRuntimeError(runtime_error, _)) =
+            withdraw_funds_receipt.err()
+        {
+            match runtime_error {
+                RuntimeError::AuthorizationError { function: _, authorization: _, error: _ } => Ok(()),
+                _ => Err(
+                    PackageError::UnauthorizedEntityCanPerformPrivilegedOperationsError(
+                        runtime_error,
+                    ),
+                ),
+            }
+        } else {
+            panic!("Should not be any other error.");
+        }
+    }
+    
+    /// Validates that when a caller is in possession of the seller badge then they can call the `withdraw_funds` 
+    /// method.
+    fn validate_seller_can_withdraw_funds(
+        &mut self,
+        component_address: &ComponentAddress,
+        seller_badge_resource_address: &ResourceAddress,
+    ) -> Result<(), PackageError> {
+        // Constructing the withdraw funds transaction
+        let withdraw_funds_transaction: SignedTransaction = TransactionBuilder::new()
+            .create_proof_from_account(seller_badge_resource_address.clone(), self.account.component_address)
+            .take_from_worktop(RADIX_TOKEN, |builder, bucket_id| {
+                builder.call_method(
+                    component_address.clone(),
+                    "withdraw_funds",
+                    args![dec!("100")],
+                )
+            })
+            .call_method_with_all_resources(self.account.component_address, "deposit_batch")
+            .build(0)
+            .sign([&self.account.private_key]);
+        self.execute_transaction(&withdraw_funds_transaction)?;
+
+        Ok(())
+    }
+
+    /// Validates that when the caller is not in possession of the seller badge then they are unable to call the 
+    /// `change_price` method.
+    fn validate_non_seller_can_not_change_price(
+        &mut self,
+        component_address: &ComponentAddress,
+        new_price: Decimal
+    ) -> Result<(), PackageError> {
+        // Constructing the withdraw funds transaction
+        let change_price_transaction: SignedTransaction = TransactionBuilder::new()
+            .take_from_worktop(RADIX_TOKEN, |builder, bucket_id| {
+                builder.call_method(
+                    component_address.clone(),
+                    "change_price",
+                    args![new_price],
+                )
+            })
+            .call_method_with_all_resources(self.account.component_address, "deposit_batch")
+            .build(0)
+            .sign([&self.account.private_key]);
+        let change_price_receipt: Result<Receipt, PackageError> =
+            self.execute_transaction(&change_price_transaction);
+
+        if let Some(PackageError::TransactionRuntimeError(runtime_error, _)) =
+            change_price_receipt.err()
+        {
+            match runtime_error {
+                RuntimeError::AuthorizationError { function: _, authorization: _, error: _ } => Ok(()),
+                _ => Err(
+                    PackageError::UnauthorizedEntityCanPerformPrivilegedOperationsError(
+                        runtime_error,
+                    ),
+                ),
+            }
+        } else {
+            panic!("Should not be any other error.");
+        }
+    }
+    
+    /// Validates that when a caller is in possession of the seller badge then they can call the `change_price` 
+    /// method.
+    fn validate_seller_can_change_price(
+        &mut self,
+        component_address: &ComponentAddress,
+        seller_badge_resource_address: &ResourceAddress,
+        new_price: Decimal
+    ) -> Result<(), PackageError> {
+        // Constructing the withdraw funds transaction
+        let change_price_transaction: SignedTransaction = TransactionBuilder::new()
+            .create_proof_from_account(seller_badge_resource_address.clone(), self.account.component_address)
+            .take_from_worktop(RADIX_TOKEN, |builder, bucket_id| {
+                builder.call_method(
+                    component_address.clone(),
+                    "change_price",
+                    args![new_price],
+                )
+            })
+            .call_method_with_all_resources(self.account.component_address, "deposit_batch")
+            .build(0)
+            .sign([&self.account.private_key]);
+        self.execute_transaction(&change_price_transaction)?;
+
+        Ok(())
     }
 }
 
@@ -361,11 +521,14 @@ pub enum PackageError {
     NoResourceAddressAtIndex,
 
     /// Represents an error where no resource could be found with the specified supply.
-    NoResourceWithSpecifiedSupply,
+    NoResourceWithSpecifiedSupply(Decimal),
 
     /// Represents an error where the team resource has invalid metadata
     InvalidMetadataOnTeamResource,
 
     /// Represents an error where the current price and the balance in the vault are not equal
     PriceBalanceMismatchError,
+
+    /// Represents an error where an unauthorized entity is able to perform operations requiring authorization.
+    UnauthorizedEntityCanPerformPrivilegedOperationsError(RuntimeError),
 }
