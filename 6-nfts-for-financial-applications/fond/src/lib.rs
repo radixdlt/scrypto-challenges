f4205de55a46f6ffdf91ccf483b70be78dec22cc
Lucas Oliveira <lucasoliveira7c@gmail.com>
2022-11-21 16:00:06 +0000

    buying and selling functions semi-implemented

--- 6-nfts-for-financial-applications/fond/src/lib.rs
+++ 6-nfts-for-financial-applications/fond/src/lib.rs
@@ -2,20 +2,52 @@ use scrypto::prelude::*;
 
 
 #[derive(NonFungibleData)]
+pub struct Asset {
+
+}
+
+#[derive(NonFungibleData)]
+pub struct InvestorAssetOwnershipBadge {
+    //...
+    share: Decimal,
+    shared_asset_badge_id: NonFungibleId
+}
+
+#[derive(NonFungibleData)]
 pub struct SharedAsset {
+    //SharedAssetBadge might make more sense
+
+
 	asset_name: String,
 	asset_description: String,
-	investment_goal: Decimal
+	investment_goal: Decimal,
+
+    original_asset_id: NonFungibleId,
+
+    bought: bool,
+    sold: bool
+
 	//accepted_token_address: ResourceAddress
-	// mut owners? 
+
+
 }
 
 
 blueprint! {
     struct Fond {
         admin_badge: Vault,
+        //FIXME: name
         current_campaigns_vault: Vault,
+
+        //actual assets
         inventory_vault: Vault,
+        
+        //Total funds collected per asset, both during investment phase as well as when the item's been sold. 
+        // K = (NonFungibleId) The ID of the shared_asset_badge
+        // V = (Vault) A vault containing all funds respective to the asset
+        collected_assets_funds: HashMap<NonFungibleId, Vault>,
+
+        dead_vaults: Vec<Vault>
     }
 
 
@@ -40,15 +72,15 @@ blueprint! {
         /// * `ComponentAddress` - The address of the `Fond` component just created.
         pub fn instantiate_fond() -> ComponentAddress {
             let admin_badge: Bucket = ResourceBuilder::new_fungible()
-            .divisibility(DIVISIBILITY_NONE)
-            .metadata("name", "Fond admin auth")
-            .initial_supply(1);
+                .divisibility(DIVISIBILITY_NONE)
+                .metadata("name", "Fond admin auth")
+                .initial_supply(1);
 
             let current_campaigns_address = ResourceBuilder::new_non_fungible()
-            .metadata("name", "Current Campaigns of company")
-            .mintable(rule!(require(admin_badge.resource_address())), LOCKED)
-            .updateable_non_fungible_data(rule!(require(admin_badge.resource_address())), LOCKED)
-            .no_initial_supply();
+                .metadata("name", "Current Campaigns of company")
+                .mintable(rule!(require(admin_badge.resource_address())), LOCKED)
+                .updateable_non_fungible_data(rule!(require(admin_badge.resource_address())), LOCKED)
+                .no_initial_supply();
 
             let current_campaigns_bucket = Bucket::new(current_campaigns_address);            
 
@@ -64,7 +96,9 @@ blueprint! {
             Self {
                 admin_badge: Vault::with_bucket(admin_badge),
                 current_campaigns_vault:  Vault::with_bucket(current_campaigns_bucket),
-                inventory_vault: Vault::with_bucket(inventory_bucket)
+                inventory_vault: Vault::with_bucket(inventory_bucket),
+                collected_assets_funds: HashMap::new(),
+                dead_vaults: Vec::new()
             }
             .instantiate()
             .globalize()
@@ -78,11 +112,14 @@ blueprint! {
         /// Checks:
         /// * **Check 1:** TODO:
 		///
+        /// ### Requires:
+        /// * `admin_badge`
+        /// 
 		/// ### Arguments:
 		/// * `asset_name` (String) - Name of the asset to be bought
 		/// * `asset_description` (String) - Description of the asset to be bought
 		/// * `investment_goal` (Decimal) - Price of the asset to be bought
-		/// * `accepted_token_address` (ResourceAddress) - Address of the token that is accepted as payment
+		/// * (LATER)`accepted_token_address` (ResourceAddress) - Address of the token that is accepted as payment
 		///
 		/// ### Returns:
 		/// * `Bucket` - A bucket containing all the listed items.
@@ -118,7 +155,11 @@ blueprint! {
             let asset = SharedAsset {
                 asset_name: asset_name, 
                 asset_description: asset_description,
-                investment_goal: investment_goal
+                investment_goal: investment_goal,
+                bought: false,
+                sold: false,
+                original_asset_id: NonFungibleId::random()
+
             };
             self.current_campaigns_vault.put(
                 campaign_resource_manager
@@ -130,6 +171,152 @@ blueprint! {
 
             self.current_campaigns_vault.take_all()
         }
+
+         
+		/// When funding for an item is achieved, the platform automatically buys the item. 
+		/// It transfers the asset from the listed assets vault to the bought assets.
+        /// 
+		/// TODO: So, everytime a user invests in an item it needs to call a function that checks that compare
+		/// current funds against the investment goal for the item
+        /// when a user invests -> call on_campaign_fulfilled() -> if true call buy_item()
+		/// 
+		/// Checks:
+		/// * TODO:
+        /// 
+        /// ### Requires:
+        /// * `admin_badge`
+		/// 
+		/// ### Arguments:
+		/// * shared_asset_badge (Bucket) - The badge representing the asset that will be bought
+		/// 
+		/// ### Returns:
+		/// * `Bucket` - The updated badge that represents the shared asset
+		pub fn add_to_inventory(&mut self, shared_asset_badge: Bucket) -> Bucket {
+
+            // 1. extract original asset NFT ID from the shared_asset_badge NFT
+
+            // Get the ID of the listed asset NFT
+            let shared_asset_badge_id: NonFungibleId = shared_asset_badge.non_fungible::<SharedAsset>().id();
+            
+            //Get the non fungible data of the listed asset NFT, so we can update it
+            let mut shared_asset_badge_non_fungible_data: SharedAsset = shared_asset_badge.non_fungible().data();
+            
+            // Retrieve the ID of the NFT of the actual asset
+            let original_asset_id = shared_asset_badge_non_fungible_data.original_asset_id.clone();
+            
+
+            // 2. simulate buying the asset: 
+            // TODO: How do we deal with non-NFT items?
+
+            //For simulation purposes we'll mint an NFT with the ID of the original asset and add it to our inventory.
+            // We then burn the funds in order to simulate payment to an external source.
+            let inventory_resource_manager = borrow_resource_manager!(self.inventory_vault.resource_address());
+
+            //FIXME: needs to be admin burnable (for simulation purposes)
+            // Asset {} was included because the function was complaining. 
+            // I don't know exactly which data should the asset contain 
+            self.admin_badge.authorize(|| self.inventory_vault.put(
+                    inventory_resource_manager.mint_non_fungible(&original_asset_id, Asset {})
+                )
+            );
+
+            //Collect the funds saved to pay for the asset
+            let mut collected_asset_funds_vault: Vault = self.collected_assets_funds.remove(&shared_asset_badge_id).unwrap();
+            let collected_asset_funds_bucket: Bucket = collected_asset_funds_vault.take_all();
+
+
+            //simulate payment to external source: burn collected funds for that asset
+            self.admin_badge.authorize(|| collected_asset_funds_bucket.burn());
+
+            //on success:
+            shared_asset_badge_non_fungible_data.bought = true;
+            
+            // Then commit our updated data to our shared_asset_badge NFT
+            self.admin_badge.authorize(|| shared_asset_badge.non_fungible().update_data(shared_asset_badge_non_fungible_data));
+
+            shared_asset_badge
+		}
+
+
+		/// The platform sells an asset from the inventory and collects the funds.
+		/// 
+		/// Checks:
+		/// * TODO:
+		/// 
+		/// ### Arguments:
+		/// * `shared_asset_badge` (Bucket) - The original asset to be sold
+		/// 
+		/// ### Returns:
+		/// * `Bucket` - The updated badge that represents the shared asset
+		pub fn sell_item(&mut self, shared_asset_badge: Bucket) -> Bucket{
+
+            //1. get the ID of the actual asset from the shared_asset_badge
+
+            //Get the non fungible data part of the shared asset badge NFT
+            let mut shared_asset_badge_non_fungible_data: SharedAsset = shared_asset_badge.non_fungible().data();
+            
+            // The NFT ID of the actual asset
+            let original_asset_id = shared_asset_badge_non_fungible_data.original_asset_id.clone();
+
+
+            //2. With that ID retrieve actual asset from inventory.
+            let original_asset: Bucket = self.inventory_vault.take_non_fungible(&original_asset_id);
+
+
+            //3. Simulate selling it: 
+            // Burn the asset (sell to external source)
+            self.admin_badge.authorize(|| original_asset.burn());
+
+            // Collect some funds greater than the original price (investment_goal)
+            // For simulation purposes, the item always sells for 110% of the original price
+            
+            // FIXME: how do we mint fungibles
+            // let accquired_funds: Bucket = 
+
+            //TODO:
+            //we then have a bucket, take the funds out of the bucket and store them in the appropriate vault
+            // (collected_assets_funds vault)
+
+
+            //on success:
+            shared_asset_badge_non_fungible_data.sold = true;
+
+            // Then commit our updated data to our shared_asset_badge NFT
+            self.admin_badge.authorize(|| shared_asset_badge.non_fungible().update_data(shared_asset_badge_non_fungible_data));
+
+            shared_asset_badge
+		}
+
+        /// The investor retrieves their cut of the sold asset according to their ownership share
+		/// 
+		/// Checks:
+		/// * **Check 1:** Check if the ID of the investor's asset ownership badge is a key of the collected_assets_funds
+        /// HashMap
+        /// //TODO:
+		/// 
+		/// ### Arguments:
+		/// * `investor_asset_ownership_badge (Bucket) - The NFT representing the investor's share of the asset.
+		/// 
+		/// ### Returns:
+		/// * `Bucket` - The investor's cut of the funds
+        pub fn retrieve_funds(&mut self, investor_asset_ownership_badge: Bucket) -> Bucket {
+            //1. Get investor's share value
+            let mut investor_ownership_badge_data: InvestorAssetOwnershipBadge 
+                = investor_asset_ownership_badge.non_fungible().data();
+            let share = investor_ownership_badge_data.share;
+            let shared_asset_badge_id = investor_ownership_badge_data.shared_asset_badge_id;
+            
+            //2. Calculate and retrieve amount owed from vault with the shared asset badge ID
+            //let mut funds_vault: ResourceAddress = self.collected_assets_funds.get(&shared_asset_badge_id);
+
+            //let investor_owed_funds: Bucket = funds_vault.take(share )
+
+            //3. Burn the investor's ownership badge
+            
+            //investor_owed_funds
+
+        }
+
     }
 
 }
