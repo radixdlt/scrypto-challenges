c4b3a3414e444ab57498ff51a75689709288ce77
Victor <victor.nguyen@hotmail.com>
2024-05-03 02:41:33 -0400

    correcting mistake

--- 8-yield-derivatives/StackedFinance/dapp/pages/components/app-footer.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup></script>
-
-<template>
-  <UContainer class="flex w-full justify-center">
-    Copyright &copy; 2024
-  </UContainer>
-</template>
-
-<style scoped></style>
--- 8-yield-derivatives/StackedFinance/dapp/pages/composables/findbyMetaData.ts
+++
@@ -1,9 +0,0 @@
-//Thsis is a way to parse metadata from entitiy details
-export const useFindbyMetaData = (metadata, key) => {
-    // Method to find the object by field_name
-    const findByFieldName = metadata.items.find(
-      (items) => items.key === key
-    )?.value;
-    // Return the reactive reference and the method
-    return findByFieldName ;
-  };
